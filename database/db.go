38e28a118c908ed0997b488ea987bda16d0b5499
Jonathan Nobrega <53060300+jonathan-nobrega@users.noreply.github.com>
2024-05-07 21:56:45 -0400

    Update db.go

--- database/db.go
+++ database/db.go
@@ -18,7 +18,7 @@ func ConectaComBancoDeDados() {
 	stringDeConexao := "host="+os.Getenv("HOST")+" user="+os.Getenv("USER")+" password="+os.Getenv("PASSWORD")+" dbname="+os.Getenv("DBNAME")+" port="+os.Getenv("PORT")+" sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
-		log.Panic("Erro ao conectar com banco de dados")
+		log.Panic("Erro ao conectar com banco de dados.")
 	}
 
 	DB.AutoMigrate(&models.Aluno{})
